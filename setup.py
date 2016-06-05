e3b492b0edc0d8fcb92e357dacd91d37a5858d05
Carlos Brandt <chbrandt@workland>
2016-06-05 17:01:50 +0200

    Slightly adjust the urls. By all means, it doesn't seam to work at RTD.

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ def replace_gavo_url():
     import glob
     import fileinput
     import re
-    pat = 'http://docs.g-vo.org/DaCHS/'
-    sub = 'http://dachs-doc.rtfd.io/en/latest/'
-    src_dir = os.path.abspath('source/')
+    pat = 'http://docs.g-vo.org/DaCHS'
+    sub = 'http://dachs-doc.rtfd.io/en/latest'
+    src_dir = os.path.abspath('docs/source/')
     rst_fls = glob.glob(os.path.join(src_dir,'*.rstx'))
     for _f in rst_fls:
         _file = fileinput.FileInput(_f, inplace=True)
         for _line in _file:
             _line = re.sub(pat,sub,_line.rstrip())
             print _line
+        _file.close()
 
 if __name__ == '__main__':
     import sys
