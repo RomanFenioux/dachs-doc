5037cdb82706b11ae1780d4722c489dbd181dd81
Carlos Brandt <chbrandt@workland>
2016-06-05 16:21:47 +0200

    Include a (fake) setup.py for RTD
    The setup duty is to replace base-URLs from GAVO to RTD when deployed
    in readthedocs. It is so far very basic, with no further structure
    to control optional parameters or check the environment around it.

---
+++ docs/setup.py
@@ -0,0 +1,20 @@
+def replace_gavo_url():
+    import os
+    import glob
+    import fileinput
+    import re
+    pat = 'http://docs.g-vo.org/DaCHS/'
+    sub = 'http://dachs-doc.rtfd.io/en/latest/'
+    src_dir = os.path.abspath('source/')
+    rst_fls = glob.glob(os.path.join(src_dir,'*.rstx'))
+    for _f in rst_fls:
+        _file = fileinput.FileInput(_f, inplace=True)
+        for _line in _file:
+            _line = re.sub(pat,sub,_line.rstrip())
+            print _line
+
+if __name__ == '__main__':
+    import sys
+    opt = sys.argv[1]
+    if opt=='install':
+        replace_gavo_url()
